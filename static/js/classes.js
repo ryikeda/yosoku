edb90dc5e5080c74fe369252ecdd9a01b341d0b1
rafael ubuntu <ryikeda@outlook.com>
2020-07-21 04:53:35 +0900

    added heroku url and updated requirements fle

--- static/js/classes.js
+++ static/js/classes.js
@@ -10,7 +10,8 @@ export {
   BASE_URL,
 };
 
-const BASE_URL = "http://127.0.0.1:5000";
+// const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "https://yosoku-app-ry.herokuapp.com/";
 
 class SignupForm {
   constructor() {
